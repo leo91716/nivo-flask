5d92b0870688fa3b87b6504669099059f8b79d0d
陳軒竹LEOCHEN <leochen@mitac.com.tw>
2023-06-28 17:07:29 +0800

    add bar chart api

--- app.py
+++ app.py
@@ -1,8 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 import pandas as pd
-app = Flask(__name__)
+import simplejson
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='build', static_url_path='/')
+CORS(app)
+
 @app.route("/")
-def hello():
+def index():
+    
+    # print(df.groupby('Country').get_group('Austria'))
+    
+        # countries.get_group(country)
+    # print(countryCollector)
+
+    return app.send_static_file('index.html')
+
+
+@app.route("/country", methods = ['POST', 'GET'])
+def country():
     df = pd.read_csv('LifeExpectancyData.csv')
-    print(df.columns)
-    return render_template('index.html')
+    countryCollector=[]
+    countries=df.groupby('Country')
+    for country in countries.groups.keys():
+        new_dict=countries.get_group(country)[['Year','Life expectancy ']].to_dict('records')
+        countryCollector.append({
+            'id':country,
+            'data':list(map(lambda x: {'y':x['Life expectancy '],'x':x['Year']},new_dict))})
+    print('Nan' in simplejson.dumps(countryCollector, ignore_nan=True))
+    return simplejson.dumps(countryCollector[-5:], ignore_nan=True)
+
+
+@app.route("/countryBar", methods = ['POST', 'GET'])
+def countryBar():
+    df = pd.read_csv('LifeExpectancyData.csv')
+    countryCollector=[]
+    countries=df[df['Year']==2014]
+    countries=countries[['infant deaths','Life expectancy ', 'Country']].to_dict('records')
+    countries=simplejson.dumps({'data':countries[-2:], 'keys':['infant deaths','Life expectancy '], 'indexBy':'Country'}, ignore_nan=True)
+    print(countries)
+    return countries
